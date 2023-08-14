44165d22dfddf9fbf0521a84f9ac200a9be9a694
AnnaKhanina <khanina.anna85@gmail.com>
2023-08-14 14:01:34 +0300

    lil fix

--- fenya/src/components/Footer.js
+++ fenya/src/components/Footer.js
@@ -5,28 +5,28 @@ export function Footer() {
     return (
         <footer className="flex h-10 items-center shadow-inner justify-center bg-gray-100">
             <ul role="list" className="list-none flex">
-                <li className="mx-2">
-                    <Link href="/contacts" className="text-lg p-2 hover:text-purple-600 focus:text-purple-600">
+                <li className="mx-2 text-lg p-2 hover:text-purple-600 focus:text-purple-600">
+                    <Link href="/contacts">
                         Наші контакти
                     </Link>
                 </li>                
-                <li className="mx-2">
-                    <Link href="/delivery" className="text-lg p-2 hover:text-purple-600 focus:text-purple-600">
+                <li className="mx-2 text-lg p-2 hover:text-purple-600 focus:text-purple-600">
+                    <Link href="/delivery">
                         Доставка і оплата
                     </Link>
                 </li>
-                <li className="mx-2">
-                    <Link href="/usesite" className="text-lg p-2 hover:text-purple-600 focus:text-purple-600">
+                <li className="mx-2 text-lg p-2 hover:text-purple-600 focus:text-purple-600">
+                    <Link href="/usesite">
                         Умови користування сайтом
                     </Link>
                 </li>
-                <li className="mx-2">
-                    <Link href="/offering" className="text-lg p-2 hover:text-purple-600 focus:text-purple-600">
+                <li className="mx-2 text-lg p-2 hover:text-purple-600 focus:text-purple-600">
+                    <Link href="/offering">
                         Договір публічної оферти та політика конфиденційності
                     </Link>
                 </li>
-                <li className="mx-2">
-                    <Link href="/blog" className="text-lg p-2 hover:text-purple-600 focus:text-purple-600">
+                <li className="mx-2 text-lg p-2 hover:text-purple-600 focus:text-purple-600">
+                    <Link href="/blog">
                         Blog: LadyNaLike
                     </Link>
                 </li>                             
