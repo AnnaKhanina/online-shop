f90f4af0fd148c74a1d39ee5f42da208d750be73
AnnaKhanina <khanina.anna85@gmail.com>
2023-08-14 14:08:40 +0300

    test title

--- fenya/src/app/layout.js
+++ fenya/src/app/layout.js
@@ -1,14 +1,17 @@
-import './input.css';
-
-export const metadata = {
-  title: 'Fenya',
-  description: 'online-shopping',
-};
+import Head from 'next/head';
+import { metadata } from './input.css'; // Импортируйте metadata из нужного места
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Head>
+          <title>{metadata.title} Fenya</title>
+          <meta name='description' content={metadata.description} />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        {children}
+      </body>
     </html>
   );
 }
--- fenya/src/app/page.js
+++ fenya/src/app/page.js
@@ -1,10 +1,15 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
+import { metadata } from './input.css'; // Импортируйте metadata из нужного места
 
 export default function Home() {
+  const pageTitle = 'Головна';
+
   return (
     <>
       <Head>
+        <title>{pageTitle} Fenya</title>
+        <meta name='description' content={metadata.description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout />
