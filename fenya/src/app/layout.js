ef90a29060e88358fb7b5cf72526c1e84a8bd5fe
AnnaKhanina <khanina.anna85@gmail.com>
2023-08-19 10:45:49 +0300

    test

--- fenya/src/app/layout.js
+++ fenya/src/app/layout.js
@@ -1,20 +1,55 @@
+// import './input.css';
 
+// export const metadata = {
+//   title: 'Fenya',
+//   description: 'online-shopping',
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
+
+// import React from 'react';
+// import Head from 'next/head';
+// import './input.css';
+
+// export const metadata = {
+//   title: 'Fenya',
+//   description: 'online-shopping',
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <Head>
+//         <title>{metadata.title ? metadata.title + ' Fenya' : 'Fenya'}</title>
+//         <meta name="description" content={metadata.description || 'Default description'} />
+//         {/* Дополнительные мета-теги, стили и другие */}
+//       </Head>
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
+
+import React from 'react';
 import Head from 'next/head';
 import './input.css';
 
+export default function RootLayout({ title, description, children }) {
+  const fullTitle = title ? `${title} Fenya` : 'Fenya';
 
-export default function RootLayout({ title, children }) {
   return (
     <html lang="en">
-      <body>
-        <Head>
-          <title>{title ? title + 'Fenya':'Fenya'}</title>
-          <meta name='description' content="online-shopping" />
-          <link rel='icon' href='/favicon.ico' />
-        </Head>
-        
-        {children}
-      </body>
+      <Head>
+        <title>{fullTitle}</title>
+        <meta name="description" content={description || 'Default description'} />
+        {/* Дополнительные мета-теги, стили и другие */}
+      </Head>
+      <body>{children}</body>
     </html>
   );
 }
--- fenya/src/app/page.js
+++ fenya/src/app/page.js
@@ -1,9 +1,27 @@
-import Layout from '../components/Layout';
+// import Layout from '../components/Layout';
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Layout title="Головна"></Layout>
+//     </>
+//   );
+// }
+
+import React from 'react';
+import RootLayout from '../components/RootLayout';
+import Header from '../components/Header';
+import Main  from '../components/Main';
+import Footer  from '../components/Footer';
 
 export default function Home() {
   return (
-    <>
-      <Layout title={'Головна'}></Layout>
-    </>
+    <RootLayout title="Home">
+     <div className="flex min-h-screen flex-col justify-between">
+        <Header />
+        <Main />
+        <Footer />
+     </div>
+    </RootLayout>
   );
 }
