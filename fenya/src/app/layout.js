ecb573fd879fdcb469f62b963087b00fbbc15f41
AnnaKhanina <khanina.anna85@gmail.com>
2023-08-20 09:35:57 +0300

    metadata

--- fenya/src/app/layout.js
+++ fenya/src/app/layout.js
@@ -1,8 +1,31 @@
 import './input.css';
 
 export const metadata = {
-  title: 'Fenya',
-  description: 'online-shopping',
+  openGraph: {
+    title: {
+      template: '%s | Fenya',
+      default: 'Fenya',
+    },
+    description: 'online store of underwear and clothes', 
+    siteName: 'Fenya',
+    locale: 'uk_UA',
+    type: 'website',
+    //     url: 'https://nextjs.org',
+    //     siteName: 'Next.js',
+    //     images: [
+    //       {
+    //         url: 'https://nextjs.org/og.png',
+    //         width: 800,
+    //         height: 600,
+    //       },
+    //       {
+    //         url: 'https://nextjs.org/og-alt.png',
+    //         width: 1800,
+    //         height: 1600,
+    //         alt: 'My custom alt',
+    //       },
+    //     ],
+  }  
 };
 
 export default function RootLayout({ children }) {
--- fenya/src/app/page.js
+++ fenya/src/app/page.js
@@ -1,5 +1,10 @@
+import * as React from 'react';
 import Head from 'next/head';
 import Layout from '../components/Layout';
+
+export const metadata = {
+  title: 'Home page',
+}
 
 export default function Home() {
   return (
@@ -7,7 +12,8 @@ export default function Home() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout />
+      <Layout>
+      </Layout>
     </>
   );
 }
