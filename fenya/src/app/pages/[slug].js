4cfe8234d8956fe3589865677fbb51ccd02ad0f9
AnnaKhanina <khanina.anna85@gmail.com>
2023-08-26 15:45:34 +0300

    test

--- fenya/src/app/pages/[slug].js
+++ fenya/src/app/pages/[slug].js
@@ -1,8 +1,28 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
+// import data from "../../utils/data";
+
+// export default function ProductScreen() {
+//     const router = useRouter(); 
+//     const { query } = router;
+//     const { slug } = query;
+//     const product = data.products.find(item => item.slug === slug)
+    
+//      if(!product) {
+//         return (
+//             <h1>Product not found</h1>
+//         )
+//      }
+
+//     return (
+//         <div>
+//             <h1>{ product.name }</h1>
+//         </div>  
+//     );
+// };
+import { withRouter } from 'next/router';
 import data from "../../utils/data";
 
-export default function ProductScreen() {
-    const router = useRouter(); 
+function ProductScreen({ router }) {
     const { query } = router;
     const { slug } = query;
     const product = data.products.find(item => item.slug === slug)
@@ -18,4 +38,12 @@ export default function ProductScreen() {
             <h1>{ product.name }</h1>
         </div>  
     );
-};
+}
+
+export default withRouter(ProductScreen);
+
+
+
+
+
+
