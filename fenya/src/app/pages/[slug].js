1e894ff8d98c453dba38fa1d21f8a7da1e2e8d64
AnnaKhanina <khanina.anna85@gmail.com>
2023-08-26 17:27:45 +0300

    +

--- fenya/src/app/pages/[slug].js
+++ fenya/src/app/pages/[slug].js
@@ -1,8 +1,34 @@
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
+import data from "../../utils/data";
+
+export const metadata = {
+    title: 'Product page',
+  }
+
+export default function ProductScreen() {
+    const router = useRouter(); 
+    const { query } = router;
+    const { slug } = query;
+    const product = data.products.find(item => item.slug === slug)
+    
+     if(!product) {
+        return (
+            <h1>Product not found</h1>
+        )
+     }
+
+    return (
+        <div>
+            <Layout>
+                <h1>{ product.name }</h1>
+            </Layout>            
+        </div>  
+    );
+};
+// import { withRouter } from 'next/router';
 // import data from "../../utils/data";
 
-// export default function ProductScreen() {
-//     const router = useRouter(); 
+// function ProductScreen({ router }) {
 //     const { query } = router;
 //     const { slug } = query;
 //     const product = data.products.find(item => item.slug === slug)
@@ -18,29 +44,9 @@
 //             <h1>{ product.name }</h1>
 //         </div>  
 //     );
-// };
-import { withRouter } from 'next/router';
-import data from "../../utils/data";
+// }
 
-function ProductScreen({ router }) {
-    const { query } = router;
-    const { slug } = query;
-    const product = data.products.find(item => item.slug === slug)
-    
-     if(!product) {
-        return (
-            <h1>Product not found</h1>
-        )
-     }
-
-    return (
-        <div>
-            <h1>{ product.name }</h1>
-        </div>  
-    );
-}
-
-export default withRouter(ProductScreen);
+// export default withRouter(ProductScreen);
 
 
 
