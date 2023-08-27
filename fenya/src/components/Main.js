1d3bbd703bdb094c5e9c0b8fc59190f0b60ba585
AnnaKhanina <khanina.anna85@gmail.com>
2023-08-27 17:37:24 +0300

    +

--- fenya/src/components/Main.js
+++ fenya/src/components/Main.js
@@ -1,15 +1,15 @@
 import data from '../utils/data';
-import { ProductItem } from './ProductItem';
+import { Product } from './Product';
 
 export function Main() {
     return (
         <main className="container mx-auto p-10 px-10 m-10 bg-slate-50 w-100 ">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 ">
             {data.products.map((product)=>(
-                <ProductItem
+                <Product
                     product={product}
                     key={product.slug}
-                ></ProductItem>
+                ></Product>
             ))}
         </div>
     </main>
