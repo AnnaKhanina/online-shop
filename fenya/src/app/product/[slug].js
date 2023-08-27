80d339e8761f2407d675ce750946f38f3e9825aa
AnnaKhanina <khanina.anna85@gmail.com>
2023-08-27 14:40:29 +0300

    test

--- fenya/src/app/product/[slug].js
+++ fenya/src/app/product/[slug].js
@@ -1,30 +1,38 @@
 import { useRouter } from 'next/router';
-import data from "../../utils/data";
+// import data from "../../utils/data";
 
 export const metadata = {
     title: 'Product page',
   }
 
-export default function ProductScreen() {
+export default function Page() {
     const router = useRouter(); 
-    const { query } = router;
-    const { slug } = query;
-    const product = data.products.find(item => item.slug === slug)
+    // const { query } = router;
+    // const { slug } = query;
+    // const product = data.products.find(item => item.slug === slug)
     
-     if(!product) {
-        return (
-            <h1>Product not found</h1>
-        )
-     }
+    //  if(!product) {
+    //     return (
+    //         <h1>Product not found</h1>
+    //     )
+    //  }
 
     return (
         <div>
             <Layout>
-                <h1>{ product.name }</h1>
+                {/* <h1>{ product.name }</h1> */}
+                <h1>{router.query.slug}</h1>
             </Layout>            
         </div>  
     );
 };
+
+// import data from "../../utils/data";
+
+// export default (req, res) => {
+//   res.status(200).json({ data: req.query.id, brand })
+// }
+
 // import { withRouter } from 'next/router';
 // import data from "../../utils/data";
 
